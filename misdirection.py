f2fae497e5e7fe589e31d720a10bfb6dc098238c
Samuel Feng <fengman@gmail.com>
2013-02-04 17:38:08 -0500

    lots of progress on text interface, and idea for checking partitions by computing paths from corner to all spots

--- misdirection.py
+++ misdirection.py
@@ -1,3 +1,21 @@
+import MisApp
+
 def main():
-    print "Hello misdirection!"
+
+    p1 = MisApp.Player((1,1))
+    p2 = MisApp.Player((8,8))
+    walls = {}
+    
+    print "******************* Testing text interface... **************"
+    inter = MisApp.TextInterface()
+    inter.prompt_player(1)
+    inter.prompt_player(2)
+    inter.set_game(p1,p2)
+    inter.set_board(p1,p2,walls)
+
+#    app = MisApp.MisApp()
+    
+
+
+    
 main()
